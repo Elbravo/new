16382d7b93419aea556ee679748854e1499a5902
Elbravo <54157270+Elbravo@users.noreply.github.com>
2022-11-05 23:02:09 +0000

    Add files via upload

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'comment' => $this->faker->paragraph(2),
+            'user_id' => rand(1, User::count()),
+            'post_id' => rand(1, Post::count()),
+        ];
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(5),
+            'user_id' => rand(1, User::count()),
+               ];
+    }
+}
